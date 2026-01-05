0be940f71a4d8e88ca91f9dd3af776cbae419e3a
Emma <EmmaRussellKilburn@gmail.com>
2026-01-05 17:21:23 +0000

    all basic run inputs

--- sveltekit-app/src/routes/+page.svelte
+++ sveltekit-app/src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { Input, Label } from "flowbite-svelte";
+	import { Fileupload, Input, Label, Textarea } from "flowbite-svelte";
 	import Info from "./components/Info.svelte";
 </script>
 
@@ -13,11 +13,26 @@
 
 	<form>
 		<div class="grid gap-6">
-		  <div>
-			  <Info tooltip="Enter a unique name for this sequencing run"></Info>
-			  <Label for="name" class="text-base">Name</Label>
-			  <Input type="text" id="name" required></Input>
-		  </div>
+		    <div>
+			    <Info tooltip="Enter a unique name for this sequencing run"></Info>
+			    <Label for="name" class="text-base">Name</Label>
+			    <Input type="text" id="name" required></Input>
+		    </div>
+			<div>
+				<Info tooltip="Select a CSV file containing the IDs and barcodes for each sample"></Info>
+				<Label for="barcodes_file" class="text-base">Barcodes file</Label>
+				<Fileupload id="barcodes_file" accept=".csv" required></Fileupload>
+			</div>
+			<div>
+				<Info tooltip="Select the folder containing sample sequencing reads from MinKnow"></Info>
+				<Label for="minknow_folder" class="text-base">MinKnow folder</Label>
+				<Fileupload id="minknow_folder" webkitdirectory="true" directory required></Fileupload>
+			</div>
+			<div>
+				<Info tooltip="Notes will be saved to the output report"></Info>
+				<Label for="notes" class="text-base">Notes</Label>
+				<Textarea id="notes" class="w-full" rows="4"></Textarea>
+			</div>
 		</div>
 	</form>
 </div>
