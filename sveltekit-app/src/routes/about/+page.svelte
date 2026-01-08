cc7b38be387b1b366c6a6e711f3ec80634c4f160
Emma <EmmaRussellKilburn@gmail.com>
2026-01-08 15:13:52 +0000

    add some placeholder content to About page

--- sveltekit-app/src/routes/about/+page.svelte
+++ sveltekit-app/src/routes/about/+page.svelte
@@ -1,16 +1,32 @@
 <script lang="ts">
-	import { resolve } from '$app/paths';
+	import pscLogo from '$lib/images/Poliovirus Sequencing Consortium.png';
 </script>
 
 <svelte:head>
-	<title>About</title>
+	<title>PiranhaNET - About</title>
 	<meta name="description" content="About PiranhaNET" />
 </svelte:head>
 
-<div>
+<div class="text-lg">
 	<h1>About PiranhaNET</h1>
-
+	<img src={ pscLogo } alt="Poliovirus Sequencing Consortium" class="float-right pr-4" />
 	<p>
-		Description of the app will go here
+		PiranhaNET is the graphical user interface for Piranha, the Polio Direct Detection by Nanopore Sequencing (DDNS)
+		analysis pipeline and reporting tool.
+	</p>
+	<p>
+		PiranhaNET has been developed by the <a href="https://polionanopore.org/" target="_blank">Poliovirus Sequencing Consortium</a>, supported by grants from the Bill and
+		Melinda Gates Foundation and the Wellcome Trust.
+	</p>
+	<p>
+		This is open-source software.<br/>
+		<a href="https://github.com/polio-nanopore/piranhaNET/" target="_blank">Github repository for PiranhaNET</a> <br/>
+		<a href="https://github.com/polio-nanopore/piranha" target="_blank">Github repository for Piranha</a>
 	</p>
 </div>
+
+<style>
+	p {
+		margin-bottom: 0.4rem;
+	}
+</style>
