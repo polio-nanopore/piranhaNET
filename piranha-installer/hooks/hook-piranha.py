26ab7c124796ecdf81a43d1d56d1b03bdf1be20d
Emma <EmmaRussellKilburn@gmail.com>
2026-02-12 19:45:46 +0000

    collect piranha datas in hook

--- piranha-installer/hooks/hook-piranha.py
+++ piranha-installer/hooks/hook-piranha.py
@@ -1,49 +1,89 @@
+import os
+import sys
 from PyInstaller import log as logging
-from PyInstaller.utils.hooks import collect_dynamic_libs, collect_submodules, conda_support
+from PyInstaller.utils.hooks import collect_dynamic_libs, collect_submodules, conda_support, collect_entry_point, collect_data_files
 
 logger = logging.getLogger(__name__)
-logger.warning("DOING PIRANHA HOOK")
+logger.info("Running piranha hook")
 
 # Use this shebang in arbitary scripts called via snakemake
 # !/home/emmarussell/dev/piranhaNET/piranha-installer/dist/piranha/_internal/piranha_dependencies/python
 
+# TODO: pin piranha version
 # TODO: consider exclude tkinter from spec rather than hacky override
-# TODO: use conda_support for source folder - or get_path as used in get_pulp_path
 # TODO: get dependencies from piranha's DEPENDENCIES list (medaka, minimap2, snakemake)
 # TODO; All the shebang python scripts will have to be changed to the right python path - it *should* be relative to the
 # script itself to pick up the right python binary, bit how to do that? These are the medaka_ scripts in piranha_dependencies - any others??
 # TODO: revisit deps in environment yml
 # TODO: add lint
 
-sm_hiddenimports = collect_submodules("snakemake")
+sm_hiddenimports = collect_submodules("snakemake") # TODO: needed?
 
 hiddenimports = sm_hiddenimports
 print(str(hiddenimports))
 
+conda_env_root = sys.prefix
+conda_bin = os.path.join(conda_env_root, "bin")
+conda_lib = os.path.join(conda_env_root, "lib")
+# TODO: rename piranha_dependencies to bin
+dependencies_dest = "piranha_dependencies"
+
+def collect_bin_files_from_package(package_name):
+    return [(file.locate(), dependencies_dest) for file in conda_support.files(package_name) if file.parts[0] == "bin"]
+
+def collect_lib_files_from_package(package_name):
+    return [(file.locate(), ".") for file in conda_support.files(package_name) if file.parts[0] == "lib"]
+
+
+def collect_bin_file(filename):
+    return (os.path.join(conda_bin, filename), dependencies_dest)
+
+# TODO: or use add_binaries
 binaries = [
-  ('/home/emmarussell/micromamba/envs/piranha-installer/bin/medaka*', 'piranha_dependencies'),
-  ('/home/emmarussell/micromamba/envs/piranha-installer/bin/minimap2*', 'piranha_dependencies'),
-  ('/home/emmarussell/micromamba/envs/piranha-installer/bin/snakemake*', 'piranha_dependencies'),
+  #('/home/emmarussell/micromamba/envs/piranha-installer/bin/medaka*', 'piranha_dependencies'),
+  *collect_bin_files_from_package("medaka"),
+  *collect_bin_files_from_package("minimap2"),
+  #('/home/emmarussell/micromamba/envs/piranha-installer/bin/snakemake*', 'piranha_dependencies'),
+  collect_bin_file("snakemake"),
   # NB cbc not in deps list but required solver for snakemake via pulp
-  ('/home/emmarussell/micromamba/envs/piranha-installer/bin/cbc*', 'piranha_dependencies'),
+  collect_bin_file("cbc"),
   # Python itself needs to be callable, from medaka_haploid_variant
-  ('/home/emmarussell/micromamba/envs/piranha-installer/bin/python*', 'piranha_dependencies'),
+  *collect_bin_files_from_package("python"),
   # dependencies used from snakemake
-  ('/home/emmarussell/micromamba/envs/piranha-installer/bin/bcftools*', 'piranha_dependencies'),
-  ('/home/emmarussell/micromamba/envs/piranha-installer/bin/bgzip*', 'piranha_dependencies'),
-  ('/home/emmarussell/micromamba/envs/piranha-installer/bin/samtools*', 'piranha_dependencies'),
-  ('/home/emmarussell/micromamba/envs/piranha-installer/bin/tabix*', 'piranha_dependencies'),
-  ('/home/emmarussell/micromamba/envs/piranha-installer/bin/mini_align*', 'piranha_dependencies'),
-  ('/home/emmarussell/micromamba/envs/piranha-installer/bin/mafft*', 'piranha_dependencies'),
+  *collect_bin_files_from_package("bcftools"),
+  *collect_bin_files_from_package("htslib"), # collects bgzip and tabix
+  #collect_bin_file("bgzip"),
+  collect_bin_file("samtools"),
+  #collect_bin_file("tabix"),
+  *collect_bin_files_from_package("mafft"),
   # required from lib (by samtools)
-  ('/home/emmarussell/micromamba/envs/piranha-installer/lib/libhts*', '.'),
-  ('/home/emmarussell/micromamba/envs/piranha-installer/lib/libgsl*', '.'),
+  #('/home/emmarussell/micromamba/envs/piranha-installer/lib/libhts*', '.'),
+  *collect_lib_files_from_package("htslib"),
+  *collect_lib_files_from_package("gsl"),
 ]
 
-#medaka_deps = conda_support.walk_dependency_tree("medaka")
-#snakemake_deps = conda_support.walk_dependency_tree("snakemake")
-#minimap2_deps = conda_support.walk_dependency_tree("minimap2")
-#print(str(medaka_deps))
+test_collect = conda_support.files("gsl")
+print("test collect")
+print(str(test_collect))
 
+#print("sys prefix")
+#print(sys.prefix)
 
+datas = [
+    *collect_data_files("piranha", include_py_files=False, subdir='data'),
+    *collect_data_files("piranha", include_py_files=True, subdir='analysis'),
+    *collect_data_files("piranha", include_py_files=True, subdir='scripts')
+]
+
+#piranha_data_files = collect_data_files("piranha", include_py_files=False, subdir='data')
+#print("DATA FILES")
+#print(str(piranha_data_files))
+
+#piranha_analysis_files = collect_data_files("piranha", include_py_files=True, subdir='analysis')
+#print("ANALYSIS FILES")
+#print(str(piranha_analysis_files))
+
+#piranha_scripts_files = collect_data_files("piranha", include_py_files=True, subdir='scripts')
+#print("SCRIPTS FILES")
+#print(str(piranha_scripts_files))
 
--- piranha-installer/piranha.spec
+++ piranha-installer/piranha.spec
@@ -1,43 +1,23 @@
 # -*- mode: python ; coding: utf-8 -*-
 
-from conda.core.envs_manager import list_all_known_prefixes
-
-# Get the root of the installed piranha package in order to copy data files etc into the installer
-# TODO: replace this with pulp pattern
-env_root = list(filter(lambda prefix: prefix.endswith("piranha-installer"), list_all_known_prefixes()))[-1]
-python_version = "3.9"
-piranha_package_root = "{env_root}/lib/python{python_version}/site-packages/piranha/".format(env_root = env_root, python_version = python_version)
-
-def get_pulp_path():
-    import pulp
-    return pulp.__path__[0]
-
 a = Analysis(
     ['entrypoint.py'],
     pathex=[],
     binaries=[],
-    datas=[
-     (piranha_package_root + 'scripts', './piranha/scripts'),
-     (piranha_package_root + 'analysis', './piranha/analysis'),
-     (piranha_package_root + 'data', './piranha/data')
-
-    ],
+    datas=[],
     hiddenimports=[
       'pulp',
       'minimap2',
       'snakemake',
       'medaka'
     ],
-    hookspath=['/home/emmarussell/dev/piranhaNET/piranha-installer/hooks'],
+    hookspath=['/home/emmarussell/dev/piranhaNET/piranha-installer/hooks'], # TODO: fix!
     hooksconfig={},
     runtime_hooks=['runtime_hook.py'],
     excludes=[],
     noarchive=False,
     optimize=0,
 )
-
-# TODO: check if we actually need to do this?
-a.datas += Tree(get_pulp_path(), prefix='pulp', excludes=["*.pyc"])
 
 pyz = PYZ(a.pure)
 
