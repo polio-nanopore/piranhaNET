eb6f94d4dab988a33e47d1832fd62c97ca1bb31b
Emma <EmmaRussellKilburn@gmail.com>
2026-02-17 15:07:53 +0000

    format

--- piranha-electron/src/renderer/src/App.svelte
+++ piranha-electron/src/renderer/src/App.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
-  import Versions from './components/Versions.svelte'
-  import piranhaLogo from './assets/piranha.svg'
+  import Versions from "./components/Versions.svelte";
+  import piranhaLogo from "./assets/piranha.svg";
 
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('run-piranha')
+  const ipcHandle = (): void => window.electron.ipcRenderer.send("run-piranha");
 </script>
 
 <img alt="logo" class="logo" src={piranhaLogo} />
 <div class="creator">Powered by electron-vite</div>
-<div class="text">
-  PiranhaNET
-</div>
+<div class="text">PiranhaNET</div>
 <div class="actions">
   <div class="action">
     <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions a11y-missing-attribute-->
