ce424b85303bbd190e370ffe2a2daacc06c50042
Emma <EmmaRussellKilburn@gmail.com>
2026-02-24 15:14:36 +0000

    add codecov token placeholder

--- piranha-electron/vitest.config.ts
+++ piranha-electron/vitest.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
           environment: "jsdom",
           clearMocks: true,
           include: ["tests/unit/renderer/**/**.{test,spec}.{js,ts}"],
-          setupFiles: ["./vitest-setup-client.ts"],
+          setupFiles: ["./vitest-setup-client.ts"]
         }
       }
     ]
