aa77c65e4b5fdd0882853fdc6adb8d2079274168
Emma <EmmaRussellKilburn@gmail.com>
2026-03-06 16:17:12 +0000

    update unit test

--- piranha-electron/tests/unit/renderer/App.svelte.test.ts
+++ piranha-electron/tests/unit/renderer/App.svelte.test.ts
@@ -1,17 +1,50 @@
-import { render, screen } from "@testing-library/svelte";
-import { describe, expect, test, beforeAll } from "vitest";
+import { render, screen, waitFor} from "@testing-library/svelte";
+import { describe, expect, test, beforeEach } from "vitest";
 import App from "../../../src/renderer/src/App.svelte";
-import { mockWindowElectron } from "../utils";
+import { mockWindowElectron , mockWindowAPI} from "../utils";
 
+// TODO use vitest-browser-svelte
 describe("App", () => {
-  beforeAll(() => {
+  beforeEach(() => {
     mockWindowElectron();
+    mockWindowAPI();
   });
 
-  test("renders as expected", () => {
+  test("renders as expected before initialized", () => {
     render(App);
     expect(screen.getByRole("img")).toHaveClass("logo");
     expect(screen.getByText(/PiranhaNET/)).toBeVisible();
-    expect(screen.getByText(/Run Piranha/)).toBeVisible();
+    expect(screen.getByText(/Initializing.../)).toBeVisible();
+  });
+
+  test("renders as expected after initialized", async () => {
+    render(App);
+
+    expect(window.api.onInitialized).toHaveBeenCalled();
+    const initialize = window.api.onInitialized.mock.calls[0][0];
+    initialize();
+
+    await waitFor(() => expect(screen.getByText(/Run Piranha/)).toBeVisible());
+    expect(screen.queryByText(/Initializing.../)).toBeNull();
+  });
+
+  test("Stream chunk and end messages are added to log", async () => {
+    render(App);
+
+    const initialize = window.api.onInitialized.mock.calls[0][0];
+    initialize();
+    await waitFor(() => expect(screen.getByText(/Run Piranha/)).toBeVisible());
+    const log = screen.getByTestId("log");
+
+    expect(window.api.onChunk).toHaveBeenCalled();
+    const addChunk = window.api.onChunk.mock.calls[0][0];
+    const encoder = new TextEncoder('utf-8');
+    addChunk(encoder.encode("test log message"));
+    await waitFor(() =>  expect(log).toHaveTextContent(/test log message/));
+
+    expect(window.api.onEnd).toHaveBeenCalled();
+    const end = window.api.onEnd.mock.calls[0][0];
+    end();
+    await waitFor(() =>  expect(log).toHaveTextContent("Piranha Run Finished"));
   });
 });
