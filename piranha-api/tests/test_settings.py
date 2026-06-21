5de1635e8599df877c6e1e9305eb938b817b6277
Emma <EmmaRussellKilburn@gmail.com>
2026-06-21 14:13:37 +0100

    settings unit tests

---
+++ piranha-api/tests/test_settings.py
@@ -0,0 +1,16 @@
+import os
+from unittest.mock import patch
+from app.settings import Settings
+
+@patch.dict("os.environ", {"INPUT_DIR": "/env/input"})
+@patch.dict("os.environ", {"OUTPUT_DIR": "/env/output"})
+def test_can_load_settings_from_env():
+    settings = Settings()
+    assert settings.input_dir == "/env/input"
+    assert settings.output_dir == "/env/output"
+
+
+def test_default_settings_when_not_in_env():
+    settings = Settings()
+    assert settings.input_dir == f"{os.getcwd()}/requests-data/input"
+    assert settings.output_dir == f"{os.getcwd()}/requests-data/output"
