26216f5c8745d0879627dd9efb09feac3df45515
Emma <EmmaRussellKilburn@gmail.com>
2026-08-17 16:14:19 +0100

    convert pull image from node to bash

---
+++ piranha-apps/scripts/pull-and-tar-piranha-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+PIRANHA_IMAGE="polionanopore/piranha"
+PIRANHA_VERSION=$(jq -r '.piranhaVersion' ../package.json)
+PIRANHA_TAG="$PIRANHA_IMAGE:$PIRANHA_VERSION"
+TARGET_DIR="./installer-resources"
+TARGET_FILE="$TARGET_DIR/piranha-docker-image.tar"
+
+echo "Making target dir if required"
+mkdir -p "$TARGET_DIR"
+echo "Pulling image"
+docker pull $PIRANHA_TAG
+echo "Saving image"
+docker save $PIRANHA_TAG -o $TARGET_FILE
+
+echo "Successfully saved image to $TARGET_FILE"
