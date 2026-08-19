3bb3d70b482b8ca686ba5f7ee9f742df45a04954
Emma Russell <EmmaRussellKilburn@gmail.com>
2026-08-19 09:41:51 +0100

    put message into pull-and-tar script re storage drivers

--- piranha-apps/scripts/pull-and-tar-piranha-docker.sh
+++ piranha-apps/scripts/pull-and-tar-piranha-docker.sh
@@ -18,7 +18,11 @@ docker image inspect $PIRANHA_TAG
 echo "Saving image"
 docker save $PIRANHA_TAG -o $TARGET_FILE
 
-echo "Zipping image"
+echo "Zipping image. Note that this step is not required when running on a machine whose docker engine"
+echo "compresses on save - this is not the case on the GHA agents. Check your docker Storage Driver "
+echo "(with 'docker info'). overlayfs compresses, overlay2 does not. If you do remove this step, e.g. "
+echo "for local installer build, remember to update the File reference in the installer scripts (e.g."
+echo "electron-installer-win.nsh) to the original .tar file, not .tar.gz"
 gzip $TARGET_FILE
 
 echo "Successfully saved image to $TARGET_FILE.gz"
