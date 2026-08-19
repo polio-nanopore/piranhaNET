3ebdfd50ff15b6fe9daf83ad1ae824c105843573
Emma Russell <EmmaRussellKilburn@gmail.com>
2026-08-19 11:26:24 +0100

    add storage driver check to pull and tar check;

--- piranha-apps/scripts/pull-and-tar-piranha-docker.sh
+++ piranha-apps/scripts/pull-and-tar-piranha-docker.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -ex
 
+STORAGE_DRIVER=$(docker info | grep -i "storage driver" | cut -d: -f2 | xargs)
+if [ "$STORAGE_DRIVER" != "overlay2" ]; then
+  echo "ERROR: Expected storage driver 'overlay2' but found '$STORAGE_DRIVER'"
+  echo "This script assumes overlay2, and performs an additional gzip step on saved docker image because overlay2 saves without compression."
+  echo "If you run this script with another storage driver which does perform compression, the gzip step will be unnecessary and the installer may fail."
+  echo "Overlay2 is the storage driver used by the github action agent for windows-latest. If this changes, please update these scripts accordingly."
+  exit 1
+fi
+
 PIRANHA_IMAGE="polionanopore/piranha"
 PIRANHA_VERSION=$(jq -r '.piranhaVersion' package.json)
 PIRANHA_TAG="$PIRANHA_IMAGE:$PIRANHA_VERSION"
@@ -12,17 +21,10 @@ mkdir -p "$TARGET_DIR"
 echo "Pulling image"
 docker pull --platform linux/amd64 $PIRANHA_TAG
 
-echo "TESTING docker inspect"
-docker image inspect $PIRANHA_TAG
-
 echo "Saving image"
 docker save $PIRANHA_TAG -o $TARGET_FILE
 
-echo "Zipping image. Note that this step is not required when running on a machine whose docker engine"
-echo "compresses on save - this is not the case on the GHA agents. Check your docker Storage Driver "
-echo "(with 'docker info'). overlayfs compresses, overlay2 does not. If you do remove this step, e.g. "
-echo "for local installer build, remember to update the File reference in the installer scripts (e.g."
-echo "electron-installer-win.nsh) to the original .tar file, not .tar.gz"
+echo "Zipping image."
 gzip $TARGET_FILE
 
 echo "Successfully saved image to $TARGET_FILE.gz"
