183c657a3532fc24bda5bb1df4e2f95d266e30cd
Emma <EmmaRussellKilburn@gmail.com>
2026-09-21 16:31:08 +0100

    fitst attempt

--- piranha-api/app/piranha_runner.py
+++ piranha-api/app/piranha_runner.py
@@ -1,21 +1,19 @@
 import asyncio
+import os
+import tempfile
 from collections.abc import AsyncGenerator
 from pathlib import Path
+from app.models import PiranhaRunOptions
 
 import aiofiles
 
-from app.models import PiranhaRunOptions
-
 POLL_WAIT = 0.2
 
-
-def safe_option(name: str, value: str):
-    if len(value.strip()) == 0:
-        return ""
+def get_config_line(name, value):
+    if isinstance(value, str):
+        return f'{name}: "{value}"\n'
     else:
-        value = value.replace(" ", "_")
-    return f"{name} {value}"
-
+        return f'{name}: {value}\n'
 
 class PiranhaRunner:
     def __init__(self, piranha_venv_path: Path):
@@ -37,15 +35,53 @@ class PiranhaRunner:
         for line in lines.split("\n"):
             yield self.log_line(run_id, line)
 
+    def write_options_to_config_file(
+        self,
+        run_options: PiranhaRunOptions,
+        barcodes_file_path: str,
+        minknow_dir_path: str,
+        output_dir_path: str
+    ):
+        with tempfile.NamedTemporaryFile(
+                mode="w+t",
+                suffix=".yaml",
+                delete=False
+            ) as config_file:
+                lines = [
+                    get_config_line("barcodes_csv", barcodes_file_path),
+                    get_config_line("readdir", minknow_dir_path),
+                    get_config_line("outdir", output_dir_path),
+                    get_config_line("runname", run_options.run_name),
+                    get_config_line("notes", run_options.notes),
+                    get_config_line("threads", run_options.threads),
+                    get_config_line("sample_type", run_options.protocol.value),
+                    get_config_line("positive_control", run_options.positive_control),
+                    get_config_line("negative_control", run_options.negative_control),
+                    get_config_line("orientation", run_options.orientation.value),
+                    get_config_line("output_prefix", run_options.output_prefix),
+                    get_config_line("all_metadata_to_header", run_options.all_metadata_to_header),
+                    get_config_line("no_temp", run_options.output_intermediate_files),
+                    get_config_line("username", run_options.user_name),
+                    get_config_line("institute", run_options.institute),
+                    get_config_line("language", run_options.lang.value),
+                    get_config_line("medaka_model", "AUTO"),
+                    #  this option ensures piranha write to our run_id output dir, not a new dir with _1 appended
+                    get_config_line("overwrite", True)
+                ]
+                config_file.writelines(lines)
+                print(("\n").join(lines))
+                return config_file.name
+
     async def run_piranha_log_generator(
         self,
         run_id: str,
         run_options: PiranhaRunOptions,
         barcodes_file_path: str,
         minknow_dir_path: str,
-        output_dir_path: str,
+        output_dir_path: str
     ) -> AsyncGenerator[str, None]:
         yield self.log_line(run_id, f"Starting run {run_options.run_name} with run id {run_id}")
+
 
         # We need to write to a log file because mafft (called fron piranha) assumes that the default stdout is
         # available, and errors if it's being piped through the subprocess. So we do not set stdout or stderr on the
@@ -54,34 +90,12 @@ class PiranhaRunner:
         # zip of the run as it may be of use.
         log_path = Path(output_dir_path) / "piranha.log"
 
-        option_vals = [
-            f"-b {barcodes_file_path}",
-            f"-i {minknow_dir_path}",
-            f"-o {output_dir_path}",
-            # run parameters
-            f"--threads {run_options.threads}",
-            safe_option("--runname", run_options.run_name),
-            safe_option("--notes", run_options.notes),
-            # run settings
-            f"--sample-type {run_options.protocol.value}",
-            safe_option("--positive-control", run_options.positive_control),
-            safe_option("--negative-control", run_options.negative_control),
-            # piranha output settings
-            f"--orientation {run_options.orientation.value}",
-            safe_option("--output-prefix", run_options.output_prefix),
-            f"{'--all-metadata-to-header' if run_options.all_metadata_to_header else ''}",
-            f"{'--no-temp' if run_options.output_intermediate_files else ''}",
-            # user settings
-            safe_option("--username", run_options.user_name),
-            safe_option("--institute", run_options.institute),
-            f"--language {run_options.lang.value}",
-            "--medaka-model AUTO",
-            # this option ensures piranha writes to our run_id output dir, not a new dir with _1 appended
-            "--overwrite ",
-        ]
-
-        options_string = " ".join(option_vals)
-        piranha_cmd = f"source {self.piranha_activate_path} && piranha {options_string} > {log_path} 2>&1"
+        config_file_path = self.write_options_to_config_file(run_options, barcodes_file_path, minknow_dir_path, output_dir_path)
+        piranha_cmd = (
+            f"source {self.piranha_activate_path} && "
+            f"piranha -c {config_file_path} "
+            f"> {log_path} 2>&1"
+        )
 
         try:
             # start non-blocking process
@@ -112,3 +126,9 @@ class PiranhaRunner:
             # TODO: Provide a way for client to more clearly know about execution error (can't set response status here
             # after start streaming). Save error to output folder, and provide /results-status response
             yield self.log_line(run_id, f"[ERROR] Exception encountered during execution: {e!s}")
+        finally:
+            if os.path.exists(config_file_path):
+                os.remove(config_file_path)
+
+
+
